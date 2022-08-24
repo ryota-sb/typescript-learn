7d8fc3f14cdfa44d6c69cdab7f06f60deddd55d7
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-24 17:04:46 +0900

    literal type

--- index.ts
+++ index.ts
@@ -18,3 +18,15 @@ coffee.size = CoffeeSize.SHORT;
 
 let unionType: number | string = 10;
 let unionTypes: (number | string)[] = ["Hello", 1000];
+
+// literal型 (決まった文字列、数字しか受けつかない型)
+
+const apple: "apple" = "apple";
+const num: 10 = 10;
+
+let size: "Small" | "Medium" | "Large" = "Small";
+
+// type alias
+
+type ClothSize = "Small" | "Medium" | "Large";
+let clothSize: ClothSize = "Large";
