bb6937f58614fbd6dbdaf2b5e1f0a004d6712b7d
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-25 20:07:43 +0900

    optional chaining

--- index.js
+++ index.js
@@ -1,51 +1,142 @@
 // Enum 列挙型
-var CoffeeSize;
-(function (CoffeeSize) {
-    CoffeeSize["SHORT"] = "SHORT";
-    CoffeeSize["TALL"] = "TALL";
-    CoffeeSize["GRANDE"] = "GRANDE";
-    CoffeeSize["VENTI"] = "VENTI";
-})(CoffeeSize || (CoffeeSize = {}));
-var coffee = {
-    hot: true,
-    size: CoffeeSize.TALL
-};
-coffee.size = CoffeeSize.SHORT;
+// enum CoffeeSize {
+//   SHORT = "SHORT",
+//   TALL = "TALL",
+//   GRANDE = "GRANDE",
+//   VENTI = "VENTI",
+// }
+// const coffee = {
+//   hot: true,
+//   size: CoffeeSize.TALL,
+// };
+// coffee.size = CoffeeSize.SHORT;
 // Union型
-var unionType = 10;
-var unionTypes = ["Hello", 1000];
+// let unionType: number | string = 10;
+// let unionTypes: (number | string)[] = ["Hello", 1000];
 // literal型 (決まった文字列、数字しか受けつかない型)
-var apple = "apple";
-var num = 10;
-var size = "Small";
-var clothSize = "Large";
+// const apple: "apple" = "apple";
+// const num: 10 = 10;
+// let size: "Small" | "Medium" | "Large" = "Small";
+// type alias
+// type ClothSize = "Small" | "Medium" | "Large";
+// let clothSize: ClothSize = "Large";
 // 関数
-function add(num1, num2) {
-    return num1 + num2;
-}
+// function add(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
 // void (戻り値なし)
-function sayHello() {
-    console.log("Hello");
-}
-console.log(sayHello());
+// function sayHello(): void {
+//   console.log("Hello");
+// }
+// console.log(sayHello());
 // 関数型
-var anotherAdd = add;
-var doubleNumber = function (num) { return num * 2; };
+// const anotherAdd: (n1: number, n2: number) => number = add;
+// const doubleNumber: (num: number) => number = (num) => num * 2;
 // callback関数に型つけ
-function doubleAndHandle(num, cd) {
-    var doubleNum = cd(num * 2);
-    console.log(doubleNum);
-}
-doubleAndHandle(21, function (doubleNum) { return doubleNum; });
+// function doubleAndHandle(num: number, cd: (num: number) => number): void {
+//   const doubleNum = cd(num * 2);
+//   console.log(doubleNum);
+// }
+// doubleAndHandle(21, (doubleNum) => doubleNum);
 // unknown型
-var unknownInput;
-var anyInput; // なんでも入る、何にでも代入できてしまう
-var text;
-if (typeof unknownInput === "string") {
-    text = unknownInput;
-}
-// never型
-function error(message) {
-    throw new Error(message);
-}
-console.log(error("This is an error"));
+// let unknownInput: unknown;
+// let anyInput: any; // なんでも入る、何にでも代入できてしまう
+// let text: string;
+// if (typeof unknownInput === "string") {
+//   text = unknownInput;
+// }
+// never型 (戻り値がundefindすらない、何も返さない時に使用する型)
+// function error(message: string): never {
+//   throw new Error(message);
+// }
+// console.log(error("This is an error"));
+// インターセクション型 （AかつB）
+// type Engineer = {
+//   name: string;
+//   role: string;
+// };
+// type Blogger = {
+//   name: string;
+//   follower: number;
+// };
+// // type EnginnerBlogger = Engineer & Blogger;
+// // const quill: EnginnerBlogger = {
+// //   name: "quill",
+// //   role: "frontend",
+// //   follower: 10000,
+// // };
+// // type NumberBooleanString = number | boolean | string;
+// // type StringNumber = string | number;
+// // type Mix = NumberBooleanString & StringNumber;
+// // type guard
+// // typeof 演算子
+// function toUpperCase(x: string | number) {
+//   if (typeof x === "string") {
+//     console.log(x.toUpperCase());
+//   }
+//   console.log("");
+// }
+// // in 演算子
+// type NomadWorker = Engineer | Blogger;
+// function describeProfile(nomadWorker: NomadWorker) {
+//   console.log(nomadWorker.name);
+//   if ("role" in nomadWorker) {
+//     console.log(nomadWorker.role);
+//   }
+//   if ("follower" in nomadWorker) {
+//     console.log(nomadWorker.follower);
+//   }
+// }
+// // instanceof
+// class Dog {
+//   speak() {
+//     console.log("bow-wow");
+//   }
+// }
+// class Bird {
+//   speak() {
+//     console.log("tweet-tweet");
+//   }
+//   fly() {
+//     console.log("flutter");
+//   }
+// }
+// type Pet = Dog | Bird;
+// function havePet(pet: Pet) {
+//   pet.speak();
+//   if (pet instanceof Bird) {
+//     pet.fly();
+//   }
+// }
+// // 型アサーション
+// const input = document.getElementById("input") as HTMLInputElement;
+// input.value = "initial input value";
+// non null assertion operator
+// const input = document.getElementById("input")!;
+// optional chaining
+// type DownloadedData = {
+//   id: number;
+//   user: {
+//     name?: {
+//       first: string;
+//       last: string;
+//     };
+//   };
+// };
+// const downloadData: DownloadedData = {
+//   id: 1,
+// };
+// console.log(downloadData.user?.name);
+// nullish coalescing
+// ?? をつける記法
+// || or演算子との違いは、0や空文字もfalseを返す
+// const userData = downloadData.user ?? "no user";
+// LoikUp type
+// type id = DownloadedData["user"]["name"];
+var Color;
+(function (Color) {
+    Color[Color["RED"] = 0] = "RED";
+    Color[Color["BLUE"] = 1] = "BLUE";
+})(Color || (Color = {}));
+var target = Color.RED;
+console.log(target);
--- index.ts
+++ index.ts
@@ -1,99 +1,193 @@
 // Enum 列挙型
 
-enum CoffeeSize {
-  SHORT = "SHORT",
-  TALL = "TALL",
-  GRANDE = "GRANDE",
-  VENTI = "VENTI",
-}
+// enum CoffeeSize {
+//   SHORT = "SHORT",
+//   TALL = "TALL",
+//   GRANDE = "GRANDE",
+//   VENTI = "VENTI",
+// }
 
-const coffee = {
-  hot: true,
-  size: CoffeeSize.TALL,
-};
+// const coffee = {
+//   hot: true,
+//   size: CoffeeSize.TALL,
+// };
 
-coffee.size = CoffeeSize.SHORT;
+// coffee.size = CoffeeSize.SHORT;
 
 // Union型
 
-let unionType: number | string = 10;
-let unionTypes: (number | string)[] = ["Hello", 1000];
+// let unionType: number | string = 10;
+// let unionTypes: (number | string)[] = ["Hello", 1000];
 
 // literal型 (決まった文字列、数字しか受けつかない型)
 
-const apple: "apple" = "apple";
-const num: 10 = 10;
+// const apple: "apple" = "apple";
+// const num: 10 = 10;
 
-let size: "Small" | "Medium" | "Large" = "Small";
+// let size: "Small" | "Medium" | "Large" = "Small";
 
 // type alias
 
-type ClothSize = "Small" | "Medium" | "Large";
-let clothSize: ClothSize = "Large";
+// type ClothSize = "Small" | "Medium" | "Large";
+// let clothSize: ClothSize = "Large";
 
 // 関数
 
-function add(num1: number, num2: number): number {
-  return num1 + num2;
-}
+// function add(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
 
 // void (戻り値なし)
 
-function sayHello(): void {
-  console.log("Hello");
-}
-console.log(sayHello());
+// function sayHello(): void {
+//   console.log("Hello");
+// }
+// console.log(sayHello());
 
 // 関数型
 
-const anotherAdd: (n1: number, n2: number) => number = add;
-const doubleNumber: (num: number) => number = (num) => num * 2;
+// const anotherAdd: (n1: number, n2: number) => number = add;
+// const doubleNumber: (num: number) => number = (num) => num * 2;
 
 // callback関数に型つけ
 
-function doubleAndHandle(num: number, cd: (num: number) => number): void {
-  const doubleNum = cd(num * 2);
-  console.log(doubleNum);
-}
-doubleAndHandle(21, (doubleNum) => doubleNum);
+// function doubleAndHandle(num: number, cd: (num: number) => number): void {
+//   const doubleNum = cd(num * 2);
+//   console.log(doubleNum);
+// }
+// doubleAndHandle(21, (doubleNum) => doubleNum);
 
 // unknown型
 
-let unknownInput: unknown;
-let anyInput: any; // なんでも入る、何にでも代入できてしまう
-let text: string;
+// let unknownInput: unknown;
+// let anyInput: any; // なんでも入る、何にでも代入できてしまう
+// let text: string;
 
-if (typeof unknownInput === "string") {
-  text = unknownInput;
-}
+// if (typeof unknownInput === "string") {
+//   text = unknownInput;
+// }
 
 // never型 (戻り値がundefindすらない、何も返さない時に使用する型)
 
-function error(message: string): never {
-  throw new Error(message);
-}
-console.log(error("This is an error"));
+// function error(message: string): never {
+//   throw new Error(message);
+// }
+// console.log(error("This is an error"));
 
 // インターセクション型 （AかつB）
 
-type Engineer = {
-  name: string;
-  role: string;
-};
+// type Engineer = {
+//   name: string;
+//   role: string;
+// };
 
-type Blogger = {
-  name: string;
-  follower: number;
-};
+// type Blogger = {
+//   name: string;
+//   follower: number;
+// };
 
-type EnginnerBlogger = Engineer & Blogger;
+// // type EnginnerBlogger = Engineer & Blogger;
 
-const quill: EnginnerBlogger = {
-  name: "quill",
-  role: "frontend",
-  follower: 10000,
-};
+// // const quill: EnginnerBlogger = {
+// //   name: "quill",
+// //   role: "frontend",
+// //   follower: 10000,
+// // };
 
-type NumberBooleanString = number | boolean | string;
-type StringNumber = string | number;
-type Mix = NumberBooleanString & StringNumber;
+// // type NumberBooleanString = number | boolean | string;
+// // type StringNumber = string | number;
+// // type Mix = NumberBooleanString & StringNumber;
+
+// // type guard
+
+// // typeof 演算子
+// function toUpperCase(x: string | number) {
+//   if (typeof x === "string") {
+//     console.log(x.toUpperCase());
+//   }
+//   console.log("");
+// }
+
+// // in 演算子
+// type NomadWorker = Engineer | Blogger;
+
+// function describeProfile(nomadWorker: NomadWorker) {
+//   console.log(nomadWorker.name);
+//   if ("role" in nomadWorker) {
+//     console.log(nomadWorker.role);
+//   }
+//   if ("follower" in nomadWorker) {
+//     console.log(nomadWorker.follower);
+//   }
+// }
+
+// // instanceof
+
+// class Dog {
+//   speak() {
+//     console.log("bow-wow");
+//   }
+// }
+
+// class Bird {
+//   speak() {
+//     console.log("tweet-tweet");
+//   }
+//   fly() {
+//     console.log("flutter");
+//   }
+// }
+
+// type Pet = Dog | Bird;
+
+// function havePet(pet: Pet) {
+//   pet.speak();
+//   if (pet instanceof Bird) {
+//     pet.fly();
+//   }
+// }
+
+// // 型アサーション
+// const input = document.getElementById("input") as HTMLInputElement;
+// input.value = "initial input value";
+
+// non null assertion operator
+
+// const input = document.getElementById("input")!;
+
+// optional chaining
+
+// type DownloadedData = {
+//   id: number;
+//   user: {
+//     name?: {
+//       first: string;
+//       last: string;
+//     };
+//   };
+// };
+
+// const downloadData: DownloadedData = {
+//   id: 1,
+// };
+
+// console.log(downloadData.user?.name);
+
+// nullish coalescing
+// ?? をつける記法
+// || or演算子との違いは、0や空文字もfalseを返す
+
+// const userData = downloadData.user ?? "no user";
+
+// LoikUp type
+
+// type id = DownloadedData["user"]["name"];
+
+enum Color {
+  RED,
+  BLUE,
+}
+
+let target = Color.RED;
+
+console.log(target);
